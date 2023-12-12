06174c9dc9e8aa954e05fe14053cf6d4086dbf35
marouane mahboub <marouane.ma7boub@gmail.com>
2023-12-12 17:22:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useReducer, useState } from "react";
+import { resultReducer } from "./reducers/resultReducer";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [firstNumberInput, setFirstNumberInput] = useState(null);
+	const [secondNumberInput, setSecondNumberInput] = useState(null);
+
+	const [result, dispatch] = useReducer(resultReducer, 0)
+
+	// EVENT HANDLERS
+	function handleSumClick() {
+		dispatch({
+			type: 'sum',
+			payload: { firstNumber: firstNumberInput, secondNumber: secondNumberInput }
+		})
+	}
+
+	function handleSubClick() {
+		dispatch({
+			type: 'subtract',
+			payload: { firstNumber: firstNumberInput, secondNumber: secondNumberInput }
+		})
+	}
+
+	function handleMultClick() {
+		dispatch({
+			type: 'multiply',
+			payload: { firstNumber: firstNumberInput, secondNumber: secondNumberInput }
+		})
+	}
+
+	function handleDivClick() {
+		dispatch({
+			type: 'divide',
+			payload: { firstNumber: firstNumberInput, secondNumber: secondNumberInput }
+		})
+	}
+
+	return (
+		<div className="App">
+			<div
+				style={{
+					height: "100vh",
+					display: "flex",
+					justifyContent: "center",
+					flexDirection: "column",
+					alignItems: "center",
+					background: "teal",
+				}}
+			>
+				{/* FIRST INPUT */}
+				<label>First Number</label>
+				<input
+					value={firstNumberInput}
+					onChange={(e) => setFirstNumberInput(e.target.value)}
+				/>
+
+				{/* SECOND INPUT */}
+				<label>Second Number</label>
+				<input
+					value={secondNumberInput}
+					onChange={(e) => setSecondNumberInput(e.target.value)}
+				/>
+
+				<button onClick={handleSumClick}>sum</button>
+
+				<button onClick={handleSubClick}>subtract</button>
+
+				<button onClick={handleMultClick}>multiply</button>
+
+				<button onClick={handleDivClick}>divide</button>
+
+				<hr />
+
+				<h2>{result}</h2>
+			</div>
+		</div>
+	);
 }
 
 export default App;
