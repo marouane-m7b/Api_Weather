e6ec6765377a062f17206fc681e9608fef686ab0
marouane mahboub <marouane.ma7boub@gmail.com>
2023-12-13 11:25:31 +0100

    Add Matrial UI + Icons + IBM Font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import "./App.css";
+import Test from "./Test";
 
+const theme = createTheme({
+	typography: {
+		fontFamily: "IBM",
+	}
+})
 
 function App() {
 
 	return (
 		<div className="App">
+			<ThemeProvider theme={theme}>
+			<Test></Test>
+			</ThemeProvider>
 		</div>
 	);
 }
